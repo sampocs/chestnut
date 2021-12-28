4531a4b56ae3283a46cd55b9da4535a604e528ec
Sam Pochyly <sam.pochyly@gmail.com>
2021-12-28 17:40:56 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Text>Main Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
